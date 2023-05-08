366dff953448614404904e4fff6b638a58e09dd0
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2023-05-08 15:50:00 +0400

    feat: add base aliases

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -2,3 +2,9 @@ export const registrationText = {
   success: 'Вы успешно зарегистрировались!',
   failure: `Что-то пошло не так! \n Попробуйте ещё раз.`,
 };
+
+
+export const BASE_ALIAS = {
+  MESTO_API: '/mesto/api',
+  MESTO_FRONT: '/mesto',
+};
